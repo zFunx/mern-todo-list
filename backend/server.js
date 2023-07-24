7e464b4d0f6a4c7beb3156e28cb471b5df7f34bb
zFunx <20762882+zFunx@users.noreply.github.com>
2023-07-24 18:56:52 +0530

    Adding backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
