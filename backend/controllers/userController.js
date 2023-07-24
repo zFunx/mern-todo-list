812f58286a3b3436cb0b14f8f49875cafcde7dd9
zFunx <20762882+zFunx@users.noreply.github.com>
2023-07-24 19:27:55 +0530

    auth route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
+// @desc    Auth user/set token
+// route    POST /api/users/auth
+// @access  Public
 const authUser = (req, res) => {
-    res.status(200).json({
-        message: ''
-    })
-}
+  res.status(200).json({
+    message: "auth user",
+  });
+};
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
-
 const port = process.env.PORT || 5000;
-
 const app = express();
 
+app.use('/api/users', userRoutes)
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
